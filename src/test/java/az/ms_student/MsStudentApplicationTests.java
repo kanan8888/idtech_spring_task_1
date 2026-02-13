b5267399958c7f1f2e248982bd61393b19d5b7b1
Kanan_Eyvazli <kanan.eyvazli.11@gmail.com>
2026-02-13 20:34:11 +0400

    .

---
+++ src/test/java/az/ms_student/MsStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ms_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
